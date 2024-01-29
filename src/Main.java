5b424e7f7f414dc9e138448d038270fa41c27bec
Milan Soucek <soucek_milan@centrum.cz>
2024-01-29 22:37:56 +0100

    sam

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
+import com.engeto.ja.computers.Employee;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee employee1=
+                new Employee("Karel Dvorak",
+                        LocalDate.of(1998,1,13),
+                        BigDecimal.ZERO, 321);
+        Employee employee2 = new Employee("Jana Mala",
+                LocalDate.of(2000, 12, 4));
+        Employee employee3 = new Employee("Anna Dvorakova",
+                LocalDate.of(1981,2,28));
+
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        employeeList.add(employee3);
+        System.out.println("Pocet zamestnancu: "+ employeeList.size());
+        for (int i = 0; i < 10; i++) {
+            employeeList.add(new Employee("Zamestnanec "+i, LocalDate.of(1990, 1, i)));
+        }
+        System.out.println("Pocet zamestnancu: " + employeeList.size());
+        System.out.println(employeeList.get(0).getName());
+        employeeList.clear();
+        employee1.setName("Karel");
+        System.out.println(employee1.getName());
+
+        employee1.setAccountState(BigDecimal.valueOf(1));
+        System.out.println(
+            employee1.canBuy(BigDecimal.TWO,5));
     }
 }
--- src/com/engeto/ja/computers/Employee.java
+++ src/com/engeto/ja/computers/Employee.java
@@ -1,2 +1,46 @@
-package com.engeto.ja.computers;public class Employee {
+package com.engeto.ja.computers;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private LocalDate birthdate;
+    private BigDecimal accountState;
+
+    private int officeNo;
+
+    public Employee(String name, LocalDate birthdate, BigDecimal accountState, int officeNo) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.accountState = accountState;
+        this.officeNo = officeNo;
+    }
+    public Employee(String name, LocalDate birthdate) {
+        this(name, birthdate, BigDecimal.ZERO, 115);
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) { this.name = name; }
+
+    public LocalDate getBirthdate() { return birthdate; }
+
+
+    public void setBirthdate(LocalDate birthdate) { this.birthdate = birthdate; }
+    public BigDecimal setAccountState() { return accountState; }
+    public void setAccountState(BigDecimal accountState) {this.accountState = accountState;}
+
+    public boolean canBuy(BigDecimal pricePerPiece, int pieces) {
+        BigDecimal totalPrice = pricePerPiece.multiply(BigDecimal.valueOf(pieces));
+        return accountState.compareTo(totalPrice) >= 0;
+    }
 }
+
+
+
+
+
+
